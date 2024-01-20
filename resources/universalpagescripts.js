7aff686d555dd5f40794dd0fd6bb1ab573cf6cef
BuildTools <unconfigured@null.spigotmc.org>
2024-01-20 04:32:46 -0600

    Updates to website infrastructure.

---
+++ resources/universalpagescripts.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    $("#page-head-code").load("https://spellbound.slothstudiorbx.com/resources/styling.html");
+    $("#navbar").load("https://spellbound.slothstudiorbx.com/resources/navbar.html");
+});
