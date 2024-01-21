b5c8a31cedb8b2a96facdfb7ed48f46659c43bce
BuildTools <unconfigured@null.spigotmc.org>
2024-01-21 03:42:54 -0600

    KB Renderer fix #5.

--- wiki/KBRenderer.js
+++ wiki/KBRenderer.js
@@ -73,7 +73,7 @@ $(document).ready(function(){
               KBTitle = KBTitle.toString();
               
               // Get KB Footer
-              var KBFooter = "<p>Author: " + KBData.Author + "</p>"
+              var KBFooter = "<br /><h3><em>Article Authored by:</em> " + KBData.Author + "</h3>"
               KBFooter = KBFooter.toString();
 
 
